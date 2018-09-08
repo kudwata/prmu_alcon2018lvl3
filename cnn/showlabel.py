0de205f2749edb63006e56e725b90ff8ed5163b9
kudwata <kud_wata@hotmail.co.jp>
2018-09-08 13:36:07 +0900

    死んだファイル書き直して短くなった

---
+++ cnn/showlabel.py
@@ -0,0 +1,17 @@
+import numpy as np
+import keras
+from keras.datasets import cifar10
+from keras.models import load_model
+
+if __name__ == '__main__':
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+    
+    X_test = x_test.astype('float32')
+    X_test /= 255.0
+
+    model = load_model('cnn_model1.h5')
+
+    labels = model.predict(X_test)
+
+    for i in range(8):
+        np.save('model1_{}.npy'.format(i), labels[i])
